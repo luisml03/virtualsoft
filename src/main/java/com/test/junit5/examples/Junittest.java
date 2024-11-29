3d0e47ffcabe14b2b984a7a169cbc8d251fc63df
luisml03 <luis.martinez@ada.co>
2024-11-29 10:56:30 -0500

    add test repo

---
+++ src/main/java/com/test/junit5/examples/Junittest.java
@@ -0,0 +1,21 @@
+package com.test.junit5.examples;
+
+import java.math.BigDecimal;
+
+public class Junittest {
+
+    private String persona;
+    private BigDecimal amount;
+    public String getPersona() {
+        return persona;
+    }
+    public void setPersona(String persona) {
+        this.persona = persona;
+    }
+    public BigDecimal getAmount() {
+        return amount;
+    }
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+}
---
+++ src/test/java/com/test/junit5/examples/JunittestTest.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+
+package com.test.junit5.examples;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+ 
+/**
+ *
+ * @author lmartinez
+ */
+public class JunittestTest {
+
+    @Test
+    void testNombreCuenta() {
+        Junittest junittest = new Junittest();
+        //junittest.setPersona("luis");
+        String esperad = "luis";
+        String real = junittest.getPersona();
+
+        assertEquals(esperad, real);
+    
+    }
+
+}
